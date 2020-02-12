0c561bbe2e50e1d861ba65600af200b27a81e040
Perehinets Marharyta <margo4owlp@gmail.com>
2020-02-12 11:44:02 +0200

    first commit

---
+++ Encode/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Encoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir_address = @"D:\trash\3 lvl\6\comp_sys\lab_1\";
+            string[] file_name = new string[] {"text_2.txt", "brake_2.txt.bz2", "rick_3.txt.bz2" };
+            char[] alphabet = new char[33] {'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и',
+        'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с',
+        'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я' };
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            for (int f = 0; f < file_name.Length; f++)
+            {
+                Console.WriteLine("{0} закодовано у Base64\n", file_name[f]);
+                string text = System.IO.File.ReadAllText(dir_address + file_name[f], Encoding.UTF8);
+                byte[] array = Encoding.UTF8.GetBytes(text);
+                MyBase64 encoder = new MyBase64(array);
+                Console.WriteLine(encoder.Encode());
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+
+        public class MyBase64
+        {
+            byte[] source;
+            int length1, length2;
+            int block;
+            int padding;
+            public MyBase64(byte[] arr)
+            {
+                source = arr;
+                length1 = arr.Length;
+                if ((length1 % 3) == 0)
+                {
+                    padding = 0;
+                    block = length1 / 3;
+                }
+                else
+                {
+                    padding = 3 - (length1 % 3);
+                    block = (length1 + padding) / 3;
+                }
+                length2 = length1 + padding;
+            }
+
+            public char[] Encode()
+            {
+                byte[] source2;
+                source2 = new byte[length2];
+                for (int x = 0; x < length2; x++)
+                {
+                    if (x < length1)
+                    {
+                        source2[x] = source[x];
+                    }
+                    else
+                    {
+                        source2[x] = 0;
+                    }
+                }
+
+                byte byte1, byte2, byte3;
+                byte temp0, temp1, temp2, temp3, temp4;
+                byte[] stack = new byte[block * 4];
+                char[] result = new char[block * 4];
+                for (int x = 0; x < block; x++)
+                {
+                    byte1 = source2[x * 3];
+                    byte2 = source2[x * 3 + 1];
+                    byte3 = source2[x * 3 + 2];
+
+                    temp1 = (byte)((byte1 & 252) >> 2);
+                    temp0 = (byte)((byte1 & 3) << 4);
+                    temp2 = (byte)((byte2 & 240) >> 4);
+                    temp2 += temp0;
+                    temp0 = (byte)((byte2 & 15) << 2);
+                    temp3 = (byte)((byte3 & 192) >> 6);
+                    temp3 += temp0;
+                    temp4 = (byte)(byte3 & 63);
+
+                    stack[x * 4] = temp1;
+                    stack[x * 4 + 1] = temp2;
+                    stack[x * 4 + 2] = temp3;
+                    stack[x * 4 + 3] = temp4;
+                }
+                for (int x = 0; x < block * 4; x++)
+                {
+                    result[x] = toResult(stack[x]);
+                }
+                switch (padding)
+                {
+                    case 0: break;
+                    case 1: result[block * 4 - 1] = '='; break;
+                    case 2:
+                        result[block * 4 - 1] = '=';
+                        result[block * 4 - 2] = '=';
+                        break;
+                    default: break;
+                }
+                return result;
+            }
+
+            private char toResult(byte b)
+            {
+                char[] s = new char[64]
+                    {  'A','B','C','D','E','F','G','H','I','J','K','L','M',
+            'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+            'a','b','c','d','e','f','g','h','i','j','k','l','m',
+            'n','o','p','q','r','s','t','u','v','w','x','y','z',
+            '0','1','2','3','4','5','6','7','8','9','+','/'};
+                if ((b >= 0) && (b <= 63))
+                {
+                    return s[(int)b];
+                }
+                else
+                {
+                    return ' ';
+                }
+            }
+        }
+    }
+}
---
+++ lab_1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir_address = @"D:\trash\3 lvl\6\comp_sys\lab_1\";
+            //string[] file_name = new string[] { "encode_64.txt", "encode_65.txt", "encode_66.txt" }; 
+            string[] file_name = new string[] { "text_1.txt.bz2", "text_2.txt.bz2", "text_3.txt.bz2" }; 2
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            //alphabet fro Ukrainian, Digits and Symbols, which are used in texts
+            //char[] alphabet = new char[84] {'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й',
+            //        'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+            //        'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я',
+            //    'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й',
+            //        'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с',
+            //        'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я',
+            //    ',',' ','(', ')', '.', ':', '-','"',
+            //    '0','1','2','3','4','5','6','7','8','9'
+            //};
+
+            //alphabet for Base64
+            char[] alphabet = new char[64]
+               {  'A','B','C','D','E','F','G','H','I','J','K','L','M',
+            'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+            'a','b','c','d','e','f','g','h','i','j','k','l','m',
+            'n','o','p','q','r','s','t','u','v','w','x','y','z',
+            '0','1','2','3','4','5','6','7','8','9','+','/'};
+
+            for (int f = 0; f < file_name.Length; f++)
+            {
+                try
+                {
+                    double[] numberOfOccurence = new double[alphabet.Length];
+                    double frequency = 0;
+                    double entropy = 0;
+                    int amount_of_letters = 0;
+
+                    string file_address = dir_address + file_name[f];
+                    FileInfo file = new FileInfo(file_address);
+                    Console.WriteLine("Файл для аналізу: " + file.Name);
+                    using (StreamReader sr = new StreamReader(file_address))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            for (int i = 0; i < alphabet.Length; i++)
+                            {
+                                var count = line.Count(x => x == alphabet[i]);
+                                numberOfOccurence[i] += count;
+                            }
+                            amount_of_letters += line.Count();
+                        }
+                    }
+                    Console.WriteLine("Загальна кількість символів файлу: " + amount_of_letters);
+                    Console.WriteLine();
+                    for (int i = 0; i < alphabet.Length; i++)
+                    {
+                        frequency = numberOfOccurence[i] / amount_of_letters;
+                        Console.WriteLine("Відносна частота появи літери \"" + alphabet[i] + "\" у тексті = " + frequency +
+                            " ; Літера присутня у тексті: " + numberOfOccurence[i] + " разів.");
+                        if (frequency != 0)
+                        {
+                            entropy += -(frequency * Math.Log(frequency, 2));
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Середня ентропія алфавіту для данного тексту: " + entropy);
+                    Console.WriteLine("Кількість інформації у тексті: " + (entropy * amount_of_letters) / 8);
+                    if ((entropy * amount_of_letters) / 8 > amount_of_letters)
+                    {
+                        Console.WriteLine("");
+                    }
+                    else if ((entropy * amount_of_letters) / 8 < amount_of_letters)
+                    {
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Кількість інформації у тексті така ж, як і розмір файлу: ");
+                    }
+                    Console.ReadKey();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
